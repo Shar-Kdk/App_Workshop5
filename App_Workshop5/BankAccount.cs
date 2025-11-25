f02d15fdf920d4a0eb0ba3f116313258ed161c63
Shar-Kdk <np05cp4a230249@iic.edu.np>
2025-11-25 08:37:04 +0545

    Added: Task-1

---
+++ App_Workshop5/BankAccount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Workshop5
+{
+    internal class BankAccount
+    {
+        private string accountNumber;
+        private double balance;
+
+        public string AccountNumber
+        {
+            get { return accountNumber; }
+        }
+
+        public double Balance
+        {
+            get { return balance; }
+            private set
+            {
+                if (value > 0)
+                    balance = value;
+            }
+        }
+
+        public BankAccount(string accountNumber, double initialBalance)
+        {
+            this.accountNumber = accountNumber;
+            Balance = initialBalance;
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+                balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > 0 && amount <= balance)
+                balance -= amount;
+        }
+    }
+}
--- App_Workshop5/Program.cs
+++ App_Workshop5/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("======== TASK 1 ========");
+            BankAccount acc = new BankAccount("12345", 1000);
+            Console.WriteLine("Account: " + acc.AccountNumber);
+            acc.Deposit(500);
+            acc.Withdraw(300);
+            Console.WriteLine("Balance: " + acc.Balance);
+            Console.WriteLine();
         }
     }
 }
